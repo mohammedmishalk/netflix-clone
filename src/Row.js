37757759e425d45d4f3651789f82cad3b8e05de1
mohammedmishalk <mishalnunu@gmail.com>
2023-01-19 11:23:35 +0530

    nav bar completed

---
+++ src/Row.js
@@ -0,0 +1,47 @@
+// import { request } from 'express'
+import {useState,useEffect} from 'react'
+import axios from "./axios"
+import "./Row.css"
+
+const baseURL = "https://image.tmdb.org/t/p/original";
+
+
+function Row({title, fetchUrl,isLargeRow}) {
+    const [movies, setMovies] = useState([])
+
+
+useEffect(() => {
+  
+    async function fetchData(){
+     const request=await axios.get(fetchUrl);
+     setMovies(request.data.results);
+    //   console.log(request)
+      return request
+    }
+
+    fetchData();
+}, [fetchUrl]);
+
+console.log(movies)
+
+
+  return (
+    <div className='row'>
+        <h2>{title}</h2>
+      <div className='row_posters'>
+        {movies.map((movie)=>{ 
+            return(
+           
+            <img
+            key={movie.id} 
+            className={`row_poster ${isLargeRow && "row_posterLarger" }' `}
+             src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+            )
+        })}
+
+      </div>
+    </div>
+  )
+}
+
+export default Row
